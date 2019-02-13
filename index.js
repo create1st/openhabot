659c53af61422d9c5386533b8190826a4d58f33e
create1st <sebastian.gil@gmail.com>
2019-02-14 00:10:46 +0100

    Support for appsecret_proof

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const http = require('http');
+const HttpStatus = require('http-status-codes');
 
 class OpenHab {
 
@@ -21,7 +22,7 @@ class OpenHab {
 
   sendHttpRequest(options, data, onData, onError) {
     var req = http.request(options, function(res) {
-      if (res.statusCode == '202') {
+      if (res.statusCode == HttpStatus.ACCEPTED) {
         onData();
       }
       console.log('STATUS: ' + res.statusCode);
@@ -44,31 +45,80 @@ class OpenHab {
 const
   fs = require('fs'),
   config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-const 
-  BootBot = require('bootbot'),
-  bot = new BootBot(config);
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json());
+const crypto = require('crypto');
+const request = require('request');
 const openHab = new OpenHab();
 
-bot.on('message', (payload, chat) => {
-  const text = payload.message.text;
-  console.log(`The user said: ${text}`);
-});
+let appSecretPoof = crypto.createHmac('sha256', config.appSecret).update(config.accessToken).digest('hex');
 
-bot.hear([/ustaw .*/i], (payload, chat) => {
-  try {
-	console.log('Setting' + payload);
-    var item = 'zwave_device_bca80c4e_node2_thermostat_setpoint_heating';
-    var newValue = '123';
-    openHab.setItemValue(item, newValue,
-      function() {
-        chat.say('Ustawiłem nową wartość ' + newValue + ' na urządzeniu ');
-      },
-      function(message) {
-		chat.say('Wystąpił problem z ustawieniem wartości: ' + message);
-      }
-    );
-  } catch(e) {
-    console.log(e);
+app.listen(config.port, () => console.log('webhook is listening on port ' + config.port));
+app.get('/webhook', (req, res) => {
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+  if (mode && token) {
+    if (mode === 'subscribe' && token === config.verifyToken) {
+      console.log('WEBHOOK_VERIFIED');
+      res.status(HttpStatus.OK).send(challenge);
+    } else {
+      res.sendStatus(HttpStatus.FORBIDDEN);      
+    }
   }
 });
-bot.start(config.port);
+app.post('/webhook', (req, res) => {  
+  let body = req.body;
+  if (body.object === 'page') {
+    body.entry.forEach(function(entry) {
+      let webhook_event = entry.messaging[0];
+      let senderPsid = webhook_event.sender.id;
+      if (webhook_event.message) {
+        handleMessage(senderPsid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(senderPsid, webhook_event.postback);
+      }
+    });
+    res.status(HttpStatus.OK).send('EVENT_RECEIVED');
+  } else {
+    res.sendStatus(HttpStatus.NOT_FOUND);
+  }
+});
+
+function handleMessage(senderPsid, message) {
+  let response;
+  if (message.text) {    
+    response = {
+      "text": `You sent the message: "${message.text}". Now send me an image!`
+    }
+  }
+  callSendAPI(senderPsid, response);   
+}
+
+function handlePostback(senderPsid, received_postback) {
+
+}
+
+function callSendAPI(senderPsid, response) {
+  let request_body = {
+	"appsecret_proof": appSecretPoof,
+    "recipient": {
+      "id": senderPsid
+    },
+    "message": response
+  }
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": config.accessToken },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (err) {
+      console.error("Unable to send message:" + err);
+    } else if (res.statusCode != HttpStatus.OK) {
+      console.log('Failed to send message:' + JSON.stringify(res));
+    }
+  }); 
+}
